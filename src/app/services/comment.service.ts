b5d253f00f423d0259725b7d2145e2713a3690de
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2022-05-12 15:50:51 +0300

    hw1

---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IComment} from "../models/IComment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  private urlComments="https://jsonplaceholder.typicode.com/comments";
+
+  constructor(private http:HttpClient) { }
+
+  getComments():Observable<IComment[]>{
+    return this.http.get<any[]>(this.urlComments)
+  }
+}
