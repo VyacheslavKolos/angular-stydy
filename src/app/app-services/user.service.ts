1dc2df4972eedfefe1b16bec451632a5905ee339
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2022-05-19 17:19:30 +0300

    hw3

---
+++ src/app/app-services/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+import {urls} from "../constants";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private Http:HttpClient) { }
+
+  getAll():Observable<IUser[]>{
+    return this.Http.get<IUser[]>(urls.users);
+  }
+
+  getUser(id:number):Observable<IUser>{
+    return this.Http.get<IUser>(urls.users+'/'+id);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './app-components/home/home.component';
+import {RouterModule, Routes} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
+import { HeaderComponent } from './app-components/header/header.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
