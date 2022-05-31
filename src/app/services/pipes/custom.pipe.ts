f05341bdad89c50925347d0467a88088e6f5a886
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2022-05-31 13:40:13 +0300

    pipes

---
+++ src/app/services/pipes/custom.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'custom'
+})
+export class CustomPipe implements PipeTransform {
+
+  transform(value: string, ...args: string[]): string {
+    return value.replace(args[0],args[1]);
+  }
+
+
+
+}
